9f27db2ca364aaae1a9b3e4c28abd4c80e01b9d8
aileen <akendrap@gmail.com>
2019-11-07 22:42:51 -0500

    file dialog included

--- cogxpy.py
+++ cogxpy.py
@@ -3,17 +3,22 @@
 # October 29, 2019  -
 
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter import filedialog
 
 # creates window from Tk class, titled CogxPy
 root = Tk()
 root.title('CogxPy')
+
+# declaring ftypes
+ftypes = [('csv file',"*.csv"), ("txt files", "*.txt")]
 
 # modifying position of frame on the screen
 root.geometry("+500+250")
 
 def loadFile():
     print("Loading the file...")
+    root.filename = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = ftypes)
+    print(root.filename)
 
        # want this to be the loading button
 buttonLoad = Button(root, text = "Load File", command = loadFile)
