ea225cca8f87852f5da2c939b3a123333b71b4d5
aileen <akendrap@gmail.com>
2019-11-25 23:48:06 -0500

    import translate

--- cogxpy.py
+++ cogxpy.py
@@ -4,6 +4,7 @@
 
 from tkinter import *
 from tkinter import filedialog
+import translate as tl
 
 # creates window from Tk class, titled CogxPy
 root = Tk()
@@ -20,6 +21,7 @@ def loadFile():
     root.filename = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = ftypes)
     print(root.filename)
     displayFilename(root.filename)
+    viewFile(filename)
 
 def displayFilename(filename):
     filename = entryLoad.insert(0, filename)
